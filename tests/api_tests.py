902ec4141259760d3b43f7815ca0f40ef95c2faa
Vadim Vzorov <vzblazer@gmail.com>
2017-04-24 08:14:19 +0200

    commit 24 apr

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -6,14 +6,13 @@ try: from urllib.parse import urlparse
 except ImportError: from urlparse import urlparse # Py2 compatibility
 from io import StringIO
 
-import sys; print(list(sys.modules.keys()))
+# import sys; print(list(sys.modules.keys())) # IDEA: WHY DO I NEED THIS?
 # Configure our app to use the testing databse
 os.environ["CONFIG_PATH"] = "tuneful.config.TestingConfig"
 
 from tuneful import app
-from tuneful import models
 from tuneful.utils import upload_path
-from tuneful.database import Base, engine, session
+from tuneful.database import Base, engine, session, Song, File, SongSchema, FileSchema
 
 class TestAPI(unittest.TestCase):
     """ Tests for the tuneful API """
@@ -37,4 +36,36 @@ class TestAPI(unittest.TestCase):
         # Delete test upload folder
         shutil.rmtree(upload_path())
 
+    def create_songs(self):
+        songB = Song()
+        fileB = File(name = 'SongB', song = songB)
+        songA = Song()
+        fileA = File(name = 'SongA', song = songA)
+        session.add_all([songA, songB])
+        session.commit()
 
+    def test_get_empty(self):
+        response = self.client.get('api/songs',
+            headers = [('Accept', 'application/json')])
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, 'application/json')
+        data = json.loads(response.data.decode('ascii'))
+        self.assertEqual (data, [])
+
+    def test_get_songs(self):
+        self.create_songs()
+        response = self.client.get('api/songs',
+            headers = [('Accept', 'application/json')])
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, 'application/json')
+        data = json.loads(response.data.decode('ascii'))
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data[0]['file']['name'], 'SongA')
+        self.assertEqual(data[1]['file']['name'], 'SongB')
+
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tuneful/api.py
+++ tuneful/api.py
@@ -1,19 +1,21 @@
 import json
 
-from flask import request, Response, url_for, send_from_directory
+from flask import request, Response, url_for, send_from_directory, jsonify
 from werkzeug.utils import secure_filename
 from jsonschema import validate, ValidationError
 
 from . import decorators
 from . import app
-from .database import session, Song, File
+from .database import session, Song, File, SongSchema, FileSchema
 from .utils import upload_path
 
-@app.route("/api/songs")
+@app.route("/api/songs", methods = ['GET'])
 @decorators.accept("application/json")
 def get_songs():
-
-    pass
-    # songs = session.query(Song)
-    # songs = songs.order_by(Song.file.name)
-    # data = json.dumps()
+    songs = session.query(Song)
+    data = []
+    for song in songs:
+        results, errors = SongSchema().dump(song.as_dictionary())
+        data.append(results)
+    results = json.dumps(data)
+    return Response(results, 200, mimetype="application/json")
